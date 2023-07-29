ecbdbc80fa9deeb03b8f043411f09ec1b8ee5a81
GauravAppdidGithub <gaurav.appdid@gmail.com>
2023-07-29 14:04:07 +0530

    changes

--- android/src/main/java/flutter/overlay/window/flutter_overlay_window/OverlayService.java
+++ android/src/main/java/flutter/overlay/window/flutter_overlay_window/OverlayService.java
@@ -245,7 +245,7 @@ public class OverlayService extends Service implements View.OnTouchListener {
 
             // Create ValueAnimator for smooth resizing
             ValueAnimator animator = ValueAnimator.ofFloat(0f, 1f);
-            animator.setDuration(300); // Set the duration of the animation in milliseconds
+            animator.setDuration(500); // Set the duration of the animation in milliseconds
 
             animator.addUpdateListener(animation -> {
                 float progress = (float) animation.getAnimatedValue();
